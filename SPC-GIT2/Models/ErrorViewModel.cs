ad1515aef2955624f2a189d39f06ce4b8b838984
Eldhose Paul <eldhose.paul@sreegcloudgmail.onmicrosoft.com>
2019-02-08 11:00:14 +0530

    commit

---
+++ SPC-GIT2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SPC_GIT2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
